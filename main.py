65d66a6bd2b972a8c230dcec0290818734fe5771
Mass Njie <mnjie2010@gmail.com>
2022-10-14 02:33:24 +0900

    add new test

--- main.py
+++ main.py
@@ -17,7 +17,7 @@ from flask import Flask, jsonify, request, abort
 JWT_SECRET = os.environ.get('JWT_SECRET')
 LOG_LEVEL = os.environ.get('LOG_LEVEL')
 
-print(JWT_SECRET)
+# print(JWT_SECRET)
 # JWT_SECRET = os.environ.get('JWT_SECRET', 'abc123abc1234')
 # LOG_LEVEL = os.environ.get('LOG_LEVEL', 'INFO')
 
